076d6d935e93c7929777003dfb47a26290cab5e8
Mohammad Banisaeid <smbl64@gmail.com>
2024-10-24 08:12:55 +0200

    Make a simple api call in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+import useSWR from 'swr';
+
+const srvAddr = import.meta.env.VITE_SERVER_ADDR;
+const fetcher = (url) => fetch(`${srvAddr}/${url}`).then((res) => res.json());
+
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const { data: user, isLoading, error } = useSWR('api/user', fetcher);
+
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
 
   return (
     <>
+      <p> Server URL is {srvAddr} </p>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Username: {user.username}</p>
+        <p>Email: {user.email}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
